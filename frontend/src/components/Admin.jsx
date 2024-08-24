555a9c9796bc30e51aa823b9f6543f0fb825ef99
Advita Bahl <21JE0063@iitism.ac.in>
2024-08-24 19:44:50 +0530

    first changes

---
+++ frontend/src/components/Admin.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import AdminNavbar from './Navbar/AdminNavbar'
+import AdminData from './AdminData'
+
+const Admin = () => {
+  return (
+    <div>
+      <AdminNavbar/>
+      <AdminData/>
+    </div>
+  )
+}
+
+export default Admin
