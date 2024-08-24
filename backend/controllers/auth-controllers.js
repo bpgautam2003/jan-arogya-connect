1797b0adeda056d89cae45acae60db03ca270e16
Brahmprakash Gautam <brahmprakash.gautam03@gmail.com>
2024-08-24 22:12:27 +0530

    inventory-apis

--- backend/controllers/auth-controllers.js
+++ backend/controllers/auth-controllers.js
@@ -1,5 +1,6 @@
 const User = require('../models/admin-model.js');
 const Reception = require('../models/reception-model.js');
+const Inventory = require('../models/inventory-model.js');
 const bcrypt = require('bcryptjs')
 
 const Home = async (req, res) => {
@@ -121,6 +122,55 @@ const receptionRegister = async(req, res) => {
 
 }
 
+const inventoryLogin = async (req, res) => {
+    try {
+        const { employee_id, password } = req.body;
+
+        //Checking if email is registered
+        const existingUser = await Inventory.findOne({ employee_id: employee_id });
+
+        if (!existingUser) {
+            return res.status(400).json({ message: "Invalid Credentials" });
+        }
+
+        console.log(existingUser)
+        //comparing password
+        const user = await bcrypt.compare(password, existingUser.password);
+
+        if (user) {
+            res.status(200).json({ message: "Login Successful", token: await existingUser.generateToken(), userId: existingUser._id.toString() });
+
+        } else {
+            res.status(401).json({ message: "Invalid Email or password" });
+        }
+    } catch (error) {
+        res.status(500).json({ message: "Internal Server Error : Login Failed" })
+    }
+}
+
+const inventoryRegister = async(req, res) => {
+    try {
+        const {name, employee_id, password } = req.body;
+
+        const userExist = await Inventory.findOne({ employee_id: employee_id });
+
+        if (userExist) {
+            return res.status(400).json({ message: "Employee Already Exists" })
+        }
+
+        //hashing the password
+        const saltround = 10; 
+        const hashedPassword = await bcrypt.hash(password, saltround);
+
+        const newUser = await Inventory.create({ name, employee_id, password:hashedPassword })
+        res.status(201).json({ message: "Registration Successful", token: await newUser.generateToken(), userId: newUser._id.toString() });
+
+    } catch (error) {
+        res.status(500).json({ message: "Internal Server Error : Registration Failed" })
+    }
+
+}
 
 
-module.exports = { adminLogin, adminRegister, getAllAdmins, receptionRegister, receptionLogin }
+
+module.exports = { adminLogin, adminRegister, getAllAdmins, receptionRegister, receptionLogin, inventoryLogin, inventoryRegister }
