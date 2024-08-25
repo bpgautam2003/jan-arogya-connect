830a140403430d8d82556f89581c536154edb0b2
Advita Bahl <21JE0063@iitism.ac.in>
2024-08-25 06:23:26 +0530

    new changes

--- frontend/src/Users/Receptionist/components/ReceptionistDashboard.jsx
+++ frontend/src/Users/Receptionist/components/ReceptionistDashboard.jsx
@@ -1,21 +1,32 @@
 import React, { useState, useEffect, useMemo } from "react";
-import { useTable, usePagination, useGlobalFilter, useFilters } from "react-table";
+import {
+  useTable,
+  usePagination,
+  useGlobalFilter,
+  useFilters,
+} from "react-table";
 import axios from "axios";
+import BedInfoCard from "../../Admin/components/BedInfoCards";
+import DoctorsTodayCard from "../../Admin/components/DoctorsTodayCard";
+import ReceptionistNavbar from "./ReceptionistNavbar";
 
 // Custom filter UI component
-const GlobalFilter = ({ preGlobalFilteredRows, globalFilter, setGlobalFilter }) => {
-    const count = preGlobalFilteredRows.length;
-  
-    return (
-      <input
-        value={globalFilter || ""}
-        onChange={(e) => setGlobalFilter(e.target.value || undefined)}
-        placeholder={'Search patients'}
-        className="border border-[#115e59] px-4 py-2 mb-4 w-2/5 float-left"
-      />
-    );
-  };
-  
+const GlobalFilter = ({
+  preGlobalFilteredRows,
+  globalFilter,
+  setGlobalFilter,
+}) => {
+  const count = preGlobalFilteredRows.length;
+
+  return (
+    <input
+      value={globalFilter || ""}
+      onChange={(e) => setGlobalFilter(e.target.value || undefined)}
+      placeholder={"Search patients"}
+      className="border border-[#115e59] px-4 py-2 mb-4 w-2/5 float-left"
+    />
+  );
+};
 
 // Custom table component
 function Table({ columns, data }) {
@@ -34,12 +45,7 @@ function Table({ columns, data }) {
     setGlobalFilter,
     preGlobalFilteredRows,
     state: { globalFilter },
-  } = useTable(
-    { columns, data },
-    useFilters,
-    useGlobalFilter,
-    usePagination
-  );
+  } = useTable({ columns, data }, useFilters, useGlobalFilter, usePagination);
 
   return (
     <div>
@@ -48,7 +54,10 @@ function Table({ columns, data }) {
         globalFilter={globalFilter}
         setGlobalFilter={setGlobalFilter}
       />
-      <table className="w-full border border-[#115e59] border-separate border-spacing-0" {...getTableProps()}>
+      <table
+        className="w-full border border-[#115e59] border-separate border-spacing-0"
+        {...getTableProps()}
+      >
         <thead>
           {headerGroups.map((headerGroup) => (
             <tr
@@ -64,7 +73,7 @@ function Table({ columns, data }) {
                   role={column.role}
                   style={column.style}
                 >
-                  {column.render('Header')}
+                  {column.render("Header")}
                 </th>
               ))}
             </tr>
@@ -87,7 +96,7 @@ function Table({ columns, data }) {
                     role={cell.role}
                     style={cell.style}
                   >
-                    {cell.render('Cell')}
+                    {cell.render("Cell")}
                   </td>
                 ))}
               </tr>
@@ -115,7 +124,6 @@ function Table({ columns, data }) {
   );
 }
 
-  
 const Modal = ({ isOpen, onClose }) => {
   const [formData, setFormData] = useState({
     name: "",
@@ -136,17 +144,19 @@ const Modal = ({ isOpen, onClose }) => {
 
   const handleSave = async () => {
     try {
-      await axios.post("http://localhost:5000/api/patient/add-patient", formData);
-      console.log('success');
+      await axios.post(
+        "http://localhost:5000/api/patient/add-patient",
+        formData
+      );
+      console.log("success");
     } catch (error) {
       console.error("Error saving data:", error);
     }
   };
-  
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(formData)
+    console.log(formData);
     handleSave(formData);
     onClose();
   };
@@ -156,10 +166,14 @@ const Modal = ({ isOpen, onClose }) => {
   return (
     <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50">
       <div className="bg-white p-6 rounded-md shadow-md w-1/3">
-        <h2 className="text-[#115e59] text-xl font-semibold mb-4">Add Patient</h2>
+        <h2 className="text-[#115e59] text-xl font-semibold mb-4">
+          Add Patient
+        </h2>
         <form onSubmit={handleSubmit}>
           <div className="mb-4">
-            <label className="block text-gray-700 text-sm font-bold mb-2 text-left">Name:</label>
+            <label className="block text-gray-700 text-sm font-bold mb-2 text-left">
+              Name:
+            </label>
             <input
               type="text"
               name="name"
@@ -170,7 +184,9 @@ const Modal = ({ isOpen, onClose }) => {
             />
           </div>
           <div className="mb-4">
-            <label className="block text-gray-700 text-sm font-bold mb-2 text-left">Patient ID:</label>
+            <label className="block text-gray-700 text-sm font-bold mb-2 text-left">
+              Patient ID:
+            </label>
             <input
               type="text"
               name="patient_id"
@@ -181,7 +197,9 @@ const Modal = ({ isOpen, onClose }) => {
             />
           </div>
           <div className="mb-4">
-            <label className="block text-gray-700 text-sm font-bold mb-2 text-left">Contact:</label>
+            <label className="block text-gray-700 text-sm font-bold mb-2 text-left">
+              Contact:
+            </label>
             <input
               type="text"
               name="contact"
@@ -193,7 +211,9 @@ const Modal = ({ isOpen, onClose }) => {
           </div>
           <div className="mb-4 grid grid-cols-3 gap-4">
             <div>
-              <label className="block text-gray-700 text-sm font-bold mb-2 text-left">Age:</label>
+              <label className="block text-gray-700 text-sm font-bold mb-2 text-left">
+                Age:
+              </label>
               <input
                 type="text"
                 name="age"
@@ -204,7 +224,9 @@ const Modal = ({ isOpen, onClose }) => {
               />
             </div>
             <div>
-              <label className="block text-gray-700 text-sm font-bold mb-2 text-left">Height:</label>
+              <label className="block text-gray-700 text-sm font-bold mb-2 text-left">
+                Height:
+              </label>
               <input
                 type="text"
                 name="height"
@@ -215,7 +237,9 @@ const Modal = ({ isOpen, onClose }) => {
               />
             </div>
             <div>
-              <label className="block text-gray-700 text-sm font-bold mb-2 text-left">Weight:</label>
+              <label className="block text-gray-700 text-sm font-bold mb-2 text-left">
+                Weight:
+              </label>
               <input
                 type="text"
                 name="weight"
@@ -227,7 +251,9 @@ const Modal = ({ isOpen, onClose }) => {
             </div>
           </div>
           <div className="mb-4">
-            <label className="block text-gray-700 text-sm font-bold mb-2 text-left">Doctor Assigned:</label>
+            <label className="block text-gray-700 text-sm font-bold mb-2 text-left">
+              Doctor Assigned:
+            </label>
             <input
               type="text"
               name="doc_assigned"
@@ -350,9 +376,10 @@ function ReceptionistDashboard() {
 
   return (
     <div className="Dashboard">
-      <h1>This is The Receptionist Dashboard</h1>
+      {/* <h1>This is The Receptionist Dashboard</h1> */}
+      <ReceptionistNavbar />
 
-      <div className="flex justify-between items-center mb-4 space-x-4">
+      {/* <div className="flex justify-between items-center mb-4 space-x-4">
         <div className="border border-[#115e59] p-4 rounded-md flex-1">
           <h3 className="text-lg font-medium text-[#115e59] mb-2">
             Bed Availability
@@ -364,6 +391,24 @@ function ReceptionistDashboard() {
             Today's Doctor
           </h3>
           <p className="text-gray-700">Details about today's doctor...</p>
+        </div>
+      </div> */}
+      <div className="container mx-auto flex flex-col md:flex-row items-center justify-between p-8 space-y-8 md:space-y-0 md:space-x-8">
+        {/* First Column: Bed Information Card */}
+        <div className="md:w-1/2 w-full">
+          <BedInfoCard totalBeds={100} availableBeds={60} occupiedBeds={40} />
+        </div>
+
+        {/* Second Column: Doctors Today Card */}
+        <div className="md:w-1/2 w-full">
+          <DoctorsTodayCard
+            doctors={[
+              { time: "09:00 AM", name: "Dr. Smith" },
+              { time: "10:30 AM", name: "Dr. Johnson" },
+              { time: "12:00 PM", name: "Dr. Brown" },
+              { time: "02:00 PM", name: "Dr. Taylor" },
+            ]}
+          />
         </div>
       </div>
 
